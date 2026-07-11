ae563e5c66a70a67d18a64bc16b3293723a88b1c
mhwagoner <michaelwagoner1971@gmail.com>
2026-07-10 20:14:15 -0700

    scenes added

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPressStart()
+    {
+        //GameManager.Instance.Score = 0;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnPressSettings()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void OnPressCredits()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void OnPressBack()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+}
